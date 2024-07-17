069eced631170e2405f8d6e7f4f66905878a16f0
ivanuria <ivan.uria@gmail.com>
2024-07-17 11:31:47 +0200

    Preparation of exercise

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -1,7 +1,56 @@
 import { useState } from 'react'
 
 const App = () => {
-  return null;
+  const anecdotes = [
+    {
+      "anecdote": "Adding manpower to a late software project makes it later!", 
+      "author": "Brooks Law"
+    },
+    {
+      "anecdote": "The best way to get a project done faster is to start sooner", 
+      "author": "Jim Highsmith"
+    },
+    {
+      "anecdote": "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.", 
+      "author": "Tom Cargill"
+    },
+    {
+      "anecdote": "Even the best planning is not so omniscient as to get it right the first time.", 
+      "author": "Fred Brooks"
+    },
+    {
+      "anecdote": "How does a project get to be a year late?... One day at a time.", 
+      "author": "Fred Brooks"
+    },
+    {
+      "anecdote": "The bearing of a child takes nine months, no matter how many women are assigned. Many software tasks have this characteristic because of the sequential nature of debugging.", 
+      "author": "Fred Brooks"
+    },
+    {
+      "anecdote": "Plan to throw one (implementation) away; you will, anyhow.", 
+      "author": "Fred Brooks"
+    },
+    {
+      "anecdote": "Every good work of software starts by scratching a developer's personal itch", 
+      "author": "Raymond"
+    },
+    {
+      "anecdote": "Perfection (in design) is achieved not when there is nothing more to add, but rather when there is nothing more to take away", 
+      "author": "Antoine de Saint-Exupery"
+    },
+    {
+      "anecdote": "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.", 
+      "author": "Martin Fowler"
+    }
+  ];
+
+  const [selected, setSelected] = useState(0);
+
+  return (
+    <main className="anecdotes">
+      {anecdotes[selected]["anecdote"]}
+    </main>
+  );
 }
 
 export default App
